b5afc8f3be0780711c6b2329f8087247189de52e
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:38:17 +0000

    verde con minima cantidad de codigo

--- lib/ValoresNutricionales/ValoresNutricionales.rb
+++ lib/ValoresNutricionales/ValoresNutricionales.rb
@@ -1,5 +1,5 @@
 class Etiqueta
-	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas, :hidratos_de_carbono, :azucares, :polialcoholes, :almidones, :fibra_alimentaria
+	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas, :hidratos_de_carbono, :azucares, :polialcoholes, :almidones, :fibra_alimentaria, :proteinas
 	def initialize(gr, gr_st,gr_mi,gr_pi,h_d_c,az,pa,am)
 		@grasa = gr
 		@grasas_saturadas = gr_st
@@ -11,5 +11,6 @@ class Etiqueta
 		@polialcoholes = pa
 		@almidones = am
 		@fibra_alimentaria = 10
+		@proteinas = 10
 	end
 end
