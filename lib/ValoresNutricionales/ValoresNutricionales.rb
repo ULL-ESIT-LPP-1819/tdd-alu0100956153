e587bf5f14693df774bb97e491a0da075e6b1771
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:26:37 +0000

    verde de nuevo al crear setter y variable azucares

--- lib/ValoresNutricionales/ValoresNutricionales.rb
+++ lib/ValoresNutricionales/ValoresNutricionales.rb
@@ -1,5 +1,5 @@
 class Etiqueta
-	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas, :hidratos_de_carbono
+	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas, :hidratos_de_carbono, :azucares
 	def initialize(gr, gr_st,gr_mi,gr_pi)
 		@grasa = gr
 		@grasas_saturadas = gr_st
@@ -7,5 +7,6 @@ class Etiqueta
 		@grasas_poliinsaturadas = gr_pi
 		
 		@hidratos_de_carbono = 15
+		@azucares= 5
 	end
 end
