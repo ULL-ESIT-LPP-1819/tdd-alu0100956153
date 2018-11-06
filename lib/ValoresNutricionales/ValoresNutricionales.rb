1f113986119f3e1b77900d2a8e97af2933a63175
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:15:42 +0000

    Verde al crear la variable y crear getter

--- lib/ValoresNutricionales/ValoresNutricionales.rb
+++ lib/ValoresNutricionales/ValoresNutricionales.rb
@@ -1,8 +1,9 @@
 class Etiqueta
-	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas
+	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas
 	def initialize()
 		@grasa = 10
 		@grasas_saturadas = 4
 		@grasas_monoinsaturadas = 3
+		@grasas_poliinsaturadas = 3
 	end
 end
