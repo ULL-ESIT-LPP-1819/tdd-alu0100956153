2ec4533910cd4c2afdcac335173d291ee6919e96
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:13:03 +0000

    Verde con minima cantidad de codigo

--- lib/ValoresNutricionales/ValoresNutricionales.rb
+++ lib/ValoresNutricionales/ValoresNutricionales.rb
@@ -1,7 +1,8 @@
 class Etiqueta
-	attr_reader :grasa,:grasas_saturadas
+	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas
 	def initialize()
 		@grasa = 10
 		@grasas_saturadas = 4
+		@grasas_monoinsaturadas = 3
 	end
 end
