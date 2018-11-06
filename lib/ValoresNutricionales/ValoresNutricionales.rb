8526b82db24f98d0dba7365ac5fd17f0070546ff
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:23:14 +0000

    Verde

--- lib/ValoresNutricionales/ValoresNutricionales.rb
+++ lib/ValoresNutricionales/ValoresNutricionales.rb
@@ -1,9 +1,11 @@
 class Etiqueta
-	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas
+	attr_reader :grasa,:grasas_saturadas, :grasas_monoinsaturadas, :grasas_poliinsaturadas, :hidratos_de_carbono
 	def initialize(gr, gr_st,gr_mi,gr_pi)
 		@grasa = gr
 		@grasas_saturadas = gr_st
 		@grasas_monoinsaturadas = gr_mi
 		@grasas_poliinsaturadas = gr_pi
+		
+		@hidratos_de_carbono = 15
 	end
 end
