2288570daaa1942e05939a4644efb9d410b66e3b
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-06 21:25:53 +0000

    rojo cuando pedimos variable azucar

--- spec/ValoresNutricionales_spec.rb
+++ spec/ValoresNutricionales_spec.rb
@@ -22,8 +22,12 @@ RSpec.describe ValoresNutricionales do
 		it "Has and displays a variable grasas_poliinsaturadas" do
 			expect(@etiqueta.grasas_poliinsaturadas).to equal(3)
 		end
+
 		it "Has and displays a variable Hidratos de carbono" do
 			expect(@etiqueta.hidratos_de_carbono).to equal(15)
+		end
+		it "Has and displays a variable azucares" do
+			expect(@etiqueta.azucares).to equal(5)
 		end
 	end
    
