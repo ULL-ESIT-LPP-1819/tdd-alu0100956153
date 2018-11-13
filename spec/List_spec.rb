44047e99066b9b6b4764a32fc65c94cfdd930a76
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-13 16:53:58 +0000

    Primer rojo pidiendo la variable de instancia head

--- spec/List_spec.rb
+++ spec/List_spec.rb
@@ -1,12 +1,34 @@
 RSpec.describe List do
 
+	before(:all) do
+		@list = List.new
+		@etiqueta = Etiqueta.new(10,4,3,3,15,5,5,5,10,10,10)
+		@n = Node.new(@etiqueta)
+	end
 
 	describe "#NODE EXIST" do
 		it "Creates a node" do
 			n = Node.new(5, nil,nil)
 			expect(n).to be_a(Struct)
 		end
-	 #	it "Contains a variable 
+	 	it "Contains a variable" do
+			n = Node.new(5,nil,nil)
+			expect(n.value).to eq(5)
+		end
+		it "Can contain a ValorNutricional element" do
+			etiqueta = Etiqueta.new(10,4,3,3,15,5,5,5,10,10,10)
+			n= Node.new(etiqueta,nil,nil)
+			expect(n.value).to be_an_instance_of(Etiqueta)
+		end	
+	end
+
+	describe "#List has basic functions" do
+		it "Creates a list" do
+			expect(@list).to be_an_instance_of(List)
+		end
+		it "Has a head" do
+			expect(@list.head).to eq(@etiqueta)
+		end
 	end
 
 
