ebf7c4845cc6df8dae535f79355ea396ff7ab343
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-13 17:12:37 +0000

    verde con minima cantidad de codigo

--- lib/ValoresNutricionales/List.rb
+++ lib/ValoresNutricionales/List.rb
@@ -1,9 +1,10 @@
 Node = Struct.new(:value, :next, :prev)
 
 class List
-	attr_reader :head
+	attr_reader :head, :tail
 	def initialize
 		@head = Node.new(5,nil,nil)
+		@tail = Node.new(5,nil,nil)
 	end
 
 end
