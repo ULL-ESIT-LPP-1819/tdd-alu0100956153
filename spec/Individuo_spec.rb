e93e83e8cb67b221c7b0aa74d124718153f78d11
Héctor Martín <alu0100956153@ull.edu.es>
2018-11-20 18:53:42 +0000

    primer rojo

--- spec/Individuo_spec.rb
+++ spec/Individuo_spec.rb
@@ -1,12 +1,15 @@
 RSpec.describe Individuo do
 	before(:all) do
-		
+		@ind = Individuo.new("Foo","McFoo")
 	end
         describe "#Individuo EXIST" do
                 it "Creates a Individuo" do
 			i = Individuo.new("Foo","McFoo")
 			expect(i).to be_a(Individuo)
 		end
+		it "Has an attribute name" do
+			expect(@ind.name).to eq("Foo")
+		end	
 	end
 end
 
