d49c5b1860d6481e74ab5c93f72abd4fbbf68f03
Héctor Martín <alu0100956153@ull.edu.es>
2018-12-18 19:55:29 +0000

    refactor y verde en punto 1 de la practica

--- spec/Menu_spec.rb
+++ spec/Menu_spec.rb
@@ -1,37 +1,45 @@
 RSpec.describe ValoresNutricionales do
 	
+	before(:all) do
+                @menu = []
+		@array_menu=[]	
+		@menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+	        @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
+                @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
+	        @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
+                @array_menu << @menu
 
+
+	end
 	describe "#Reperesenta las valoraciones" do
-		menu = []
-		array_menu=[]
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
-		menu << Etiqueta.new(5,4,3,3,5,5,5,5,5,5,5)
 
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
-		array_menu << menu
 
 		it "has a size of 10 as an array" do
-			expect(array_menu.size).to eq(10)
+			expect(@array_menu.size).to eq(10)
 		end
 		
 		it "is in fact a array" do
-			expect(array_menu.is_a? Array).to eq(true)
+			expect(@array_menu.is_a? Array).to eq(true)
+		end
+
+		it "has a list of arrays of size 10" do
+			
+			
 		end
 	end
 
